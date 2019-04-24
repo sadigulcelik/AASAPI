cd9421f5e1b9de26780fed781b7e0ee681efac5d
Ting <andrew.ting19@mbpr1315-3260.local>
2019-04-24 11:37:25 -0400

    Andrew weds

--- controllers/user.js
+++ controllers/user.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var fs = require("fs");
 var router = express.Router();
-var Users = require('../models/User');
+//var Users = require('../models/User');
+var Developer = require('.../models/Developer');
 var dataJS = require('../models/data');
-var Villains = require('../models/Villain');
+//var Villains = require('../models/Villain');
 var userName;
 var userPSWD;
 
@@ -144,10 +145,10 @@ router.put('/user/:id', function (req, res) {
   var feedback = {
     failure:0
   }
-  
+
   res.status(200);
   res.setHeader('Content-Type', 'text/html')
-  
+
   if (u.name==null||u.name==""||u.pswd==null||u.pswd==""||u.first==null||u.first==""||u.last==null||u.last==""){
       dataJS.log("inv");
       result= false;
@@ -203,53 +204,6 @@ router.get('/user/:id/results', function(request, response){
     weapon: request.query.weapon,
     villain: request.query.villain
   };
-
-  var villainPrevious=fs.readFileSync("data/villainPrevious.txt",'utf8');
-  var userPrevious=fs.readFileSync("data/userPrevious.txt",'utf8');
-
-  if (user_data.weapon=="error"||user_data.villain=="error"){
-    response.redirect("/error");
-  } else{
-    var villainWeapon= "";
-    var arr = Users.handleThrow(user_data.weapon, user_data.villain, villainWeapon, villainPrevious,userPrevious);
-    user_data["result"] = arr[0];
-    user_data["response"] =arr[1];
-
-    Users.getUser(user_data.name, function(user_obj) {
-      user_obj.total =parseInt(user_obj.total)+1
-      user_obj[user_data.weapon]=parseInt(user_obj[user_data.weapon])+ 1
-      switch(user_data["result"]){
-        case "won":
-          user_obj.wins =parseInt(user_obj.wins)+1
-          break;
-        case "lost":
-          user_obj.losses =parseInt(user_obj.losses)+1
-          break;
-      }
-      var user_array = [user_obj.name, user_obj.pswd, user_obj.total, user_obj.wins, user_obj.losses, user_obj.rock, user_obj.paper, user_obj.scissors, user_obj.first, user_obj.last, user_obj.creation, user_obj.update]
-      Villains.getVillain(user_data.villain, function(villain_obj){
-        villain_obj.total =parseInt(villain_obj.total)+1
-        villain_obj[user_data["response"]]=parseInt(villain_obj[user_data["response"]])+1
-        switch(user_data["result"]){
-            case "won":
-              villain_obj.losses = parseInt(villain_obj.losses)+1
-              break;
-            case "lost":
-              villain_obj.wins = parseInt(villain_obj.wins)+1
-              break;
-          }
-        var villain_array = [villain_obj.name, villain_obj.total, villain_obj.wins, villain_obj.losses, villain_obj.rock, villain_obj.paper, villain_obj.scissors]
-        Users.updateUser(user_obj.name, user_array, function(){
-          Villains.updateVillain(villain_obj.name, villain_array, function(){
-            response.status(200);
-            response.setHeader('Content-Type', 'text/html')
-            dataJS.increment("results")
-            response.render('results',{page:request.url, user:user_data, title:"results"});
-          });
-        });
-      });
-    });
-  }
 });
 
 module.exports = router;
